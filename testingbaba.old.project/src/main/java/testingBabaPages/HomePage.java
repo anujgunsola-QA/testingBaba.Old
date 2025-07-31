523de9dae6c71040b38450889dbcb57fffdd8238
anujgunsola-QA <anujgunsola91@gmail.com>
2025-07-31 15:20:29 +0530

    Add files via upload

---
+++ testingbaba.old.project/src/main/java/testingBabaPages/HomePage.java
@@ -0,0 +1,15 @@
+package testingBabaPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HomePage {
+		WebDriver driver = new ChromeDriver();
+		
+		@Test
+		void launchBrowser() {
+			driver.manage().window().maximize();
+			driver.get("https://testingbaba.com/old/");
+		}
+}
